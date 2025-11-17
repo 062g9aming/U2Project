b965692065ddde931153746fa10ed74e0b263aa4
JustinShek <e@e>
2025-11-17 14:49:35 -0500

    x

--- src/Entity.java
+++ src/Entity.java
@@ -1,27 +1,43 @@
+import java.util.Random;
 public class Entity {
-    private String entity;
-    private int hP;
+    private static String entity;
+    private static int hP;
     private static int aTK;
-    private int dEF;
-    private int luckiness;
-    private int cR;
+    private static int dEF;
+    private static int luck;
+    private static int cR;
+    private static int baseHP;
+    private static int baseATK;
+    private static int baseDEF;
+    private static int baseLuck;
+    private static int baseCR;
 
-    public Entity(String name, String title, int maxHP, int ATK, int DEF, int luck, int criticalChance, String weapon)
+
+    public Entity(String name, String title, int maxHP, int ATK, int DEF, int luck, int criticalRate, String weapon)
     {
         hP = maxHP;
-        aTK = ATK;
-        dEF = DEF;
-        luckiness = luck;
-        cR = criticalChance;
+        Entity.aTK = ATK;
+        this.dEF = DEF;
+        this.luck = luck;
+        cR = criticalRate;
 
         System.out.println(name);
         System.out.println("-------" + title + "-------");
         System.out.println("HP: " + maxHP + "   |   ATK: " + ATK + "   |   DEF: " + DEF);
     }
 
-    public static int attackPlayer()
+    public double attackPlayer()
     {
-        return(aTK);
+        Random r = new Random();
+        //used to randomize base ATK
+        double dMG = (aTK * ((r.nextInt(41) + 80) / 100.0));
+        int generated_Percent = r.nextInt(101);
+        if (cR >= generated_Percent)
+        {
+            System.out.println("Crit!");
+            dMG *= 2;
+        }
+        return(dMG);
     }
 
     
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Entity Player = new Entity("Das", "The Dass Boss",10, 1, 2, 0, 5, "Beginners");
+        Random r = new Random();
+        double dMG = (aTK * ((r.nextInt(41) + 80) / 100.0));
+        Entity Player = new Entity("YOU", "The Dass Boss",10, 100000000, 2, 0, 100, "Beginners");
+        Entity bud = new Entity("Das", "The Dass Boss",10, 1, 2, 0, 100, "Beginners");
+        System.out.println(Player.attackPlayer());
     }
 }
