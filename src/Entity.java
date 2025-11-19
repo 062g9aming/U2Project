3e8fe37367437c9300a8b9d3c782a1df891a7d57
062g9aming <justins764@nycstudents.net>
2025-11-19 14:51:11 -0500

    Add files via upload
    11/19 commit

--- src/Entity.java
+++ src/Entity.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class Entity {
     private String entity;
+    private String title;
     private int hP;
     private int aTK;
     private int dEF;
@@ -18,31 +19,31 @@ public class Entity {
 
     public Entity(String name, String title, int maxHP, int ATK, int DEF, int luck, int criticalRate, String weapon)
     {
+        //display stats at max
         entity = name;
+        this.title = title;
         hP = maxHP;
         aTK = ATK;
-        this.dEF = DEF;
+        dEF = DEF;
         this.luck = luck;
         cR = criticalRate;
+        //display stats at base/max
+        baseHP = maxHP;
+        baseATK = ATK;
+        baseDEF = DEF;
+        baseLuck = luck;
+        baseCR = criticalRate;
 
         System.out.println(name);
         System.out.println("-------" + title + "-------");
         System.out.println("HP: " + maxHP + "   |   ATK: " + ATK + "   |   DEF: " + DEF);
     }
 
-    public int attackPlayer()
-    {
-        //used to randomize base ATK
-        int dMG = (int)(aTK * ((r.nextInt(41) + 80) / 100.0));
-        int generated_Percent = r.nextInt(101);
-        if (cR >= generated_Percent)
-        {
-            System.out.println("Crit!");
-            dMG *= 2;
-        }
-        return(dMG);
-    }
 
+
+
+
+    //stat updates
     public void dmgReceived(int RawDMG) {
         int incomingDMG = (int) (RawDMG * (100 - dEF) / 100.0);
         if (luck <= r.nextInt(101))
@@ -54,16 +55,56 @@ public class Entity {
             System.out.println("Ouch! " + entity + " received " + incomingDMG + " DMG!");
             hP -= incomingDMG;
         }
+        statsCurrent();
     }
 
     public void statsCurrent()
     {
-        System.out.println(name);
+        System.out.println(entity);
         System.out.println("-------" + title + "-------");
-        System.out.println("HP: " + maxHP + "   |   ATK: " + ATK + "   |   DEF: " + DEF);
+        System.out.println("HP: " + hP + "   |   ATK: " + aTK + "   |   DEF: " + dEF);
+        if (hP <= 0)
+        {
+            System.out.println(entity + " has been slain...");
+        }
     }
 
 
-    
 
+
+
+    //movesets
+    public int attackM1(int variance)
+    {
+        //used to randomize base ATK
+        int dMG = (int)(aTK * ((r.nextInt(80 - variance, 80 + variance+1)) / 100.0));
+        int generated_Percent = r.nextInt(101);
+        if (cR >= generated_Percent)
+        {
+            System.out.println("Crit!");
+            dMG *= 2;
+        }
+        return(dMG);
+    }
+
+    public void healM2(int percent, int variance)
+    {
+        int healing = (int)(baseHP * ((r.nextInt(percent - variance, percent+variance+1)  / 100.0)));
+        int generated_Percent = r.nextInt(101);
+        if (luck >= generated_Percent)
+        {
+            System.out.println("Crit Heal!");
+            healing *= 1.5;
+        }
+        System.out.println(entity + " recovered " + healing + " HP!");
+        if ((hP + healing) > baseHP)
+        {
+            hP = baseHP;
+        }
+        else
+        {
+            hP += healing;
+        }
+        statsCurrent();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        Entity Player = new Entity("YOU", "The Dass Boss",10, 1000, 2, 77, 50, "Beginners");
-        Entity Buddy = new Entity("heuuu", "The Dass Boss",10, 10, 2, 0, 50, "Beginners");
-        int ouch = Buddy.attackPlayer();
-        Player.dmgReceived(ouch);
+        Entity player = new Entity("YOU", "The Dass Boss",1000, 1000, 1, 77, 50, "Beginners");
+        Entity enemy = new Entity("heuuu", "The Dass Boss",10, 400, 2, 0, 50, "Beginners");
+        for (int i = 1; i != 5; i++) {
+            int ouch = enemy.attackM1(20);
+            player.dmgReceived(ouch);
+            player.healM2(20, 3);
+        }
     }
 }
