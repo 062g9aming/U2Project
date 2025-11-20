ce67ff29e2874d4508ac4e302440249008927fdb
062g9aming <justins764@nycstudents.net>
2025-11-20 14:51:24 -0500

    Add files via upload
    11/20 commit

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Entity {
     private String entity;
@@ -13,6 +14,7 @@ public class Entity {
     private int baseDEF;
     private int baseLuck;
     private int baseCR;
+    private int ultCharge;
 
     Random r = new Random();
 
@@ -69,27 +71,70 @@ public class Entity {
         }
     }
 
+    //permanent stat change
+    public void statMutation(int HP, int ATK, int DEF, int luck, int criticalRate)
+    {
+        baseHP += HP;
+        baseATK += ATK;
+        baseDEF += DEF;
+        baseLuck += luck;
+        baseCR += criticalRate;
+    }
+
+    public void statReset()
+    {
+        hP = baseHP;
+        aTK = baseATK;
+        dEF = baseDEF;
+        luck = baseLuck;
+        cR = baseCR;
+    }
+
+
+    //create new opponent
+    public void newEntity(String name, String title, int maxHP, int ATK, int DEF, int luck, int criticalRate, String weapon)
+    {
+        entity = name;
+        this.title = title;
+        hP = maxHP;
+        aTK = ATK;
+        dEF = DEF;
+        this.luck = luck;
+        cR = criticalRate;
+        //display stats at base/max
+        baseHP = maxHP;
+        baseATK = ATK;
+        baseDEF = DEF;
+        baseLuck = luck;
+        baseCR = criticalRate;
+
+        System.out.println(name);
+        System.out.println("-------" + title + "-------");
+        System.out.println("HP: " + maxHP + "   |   ATK: " + ATK + "   |   DEF: " + DEF);
+    }
+
 
 
 
 
     //movesets
-    public int attackM1(int variance)
+    public int attackM1(int variance, int base, int multiplier)
     {
         //used to randomize base ATK
-        int dMG = (int)(aTK * ((r.nextInt(80 - variance, 80 + variance+1)) / 100.0));
+        int dMG = (int)((multiplier) * (aTK + base) * ((r.nextInt(80 - variance, 80 + variance+1)) / 100.0));
         int generated_Percent = r.nextInt(101);
         if (cR >= generated_Percent)
         {
             System.out.println("Crit!");
             dMG *= 2;
         }
+        System.out.println(entity + " dealt " + dMG + " DMG!");
         return(dMG);
     }
 
-    public void healM2(int percent, int variance)
+    public void healM2(int percent, int variance, int base, int multiplier)
     {
-        int healing = (int)(baseHP * ((r.nextInt(percent - variance, percent+variance+1)  / 100.0)));
+        int healing = (int)((multiplier) * (baseHP + base) * ((r.nextInt(percent - variance, percent+variance+1)  / 100.0)));
         int generated_Percent = r.nextInt(101);
         if (luck >= generated_Percent)
         {
@@ -107,4 +152,36 @@ public class Entity {
         }
         statsCurrent();
     }
+
+    //code this such that during buff rest will apply right after
+    public int boostDMGM3(int multiplier, int duration)
+    {
+        if (duration > 0)
+        {
+            duration -= 1;
+            return(multiplier);
+        }
+        return(0);
+    }
+
+
+
+
+
+    //ultset
+
+    public int nukeU1(int variance, int base, int multiplier)
+    {
+        //triple attack but takes personal dmg
+        int dMG = (int)((3 * multiplier) * (aTK + base) * ((r.nextInt(80 - variance, 80 + variance+1)) / 100.0));
+        int generated_Percent = r.nextInt(101);
+        if (cR >= generated_Percent)
+        {
+            System.out.println("Crit!");
+            dMG *= 2;
+        }
+        System.out.println(entity + " dealt " + dMG + " Nuke DMG!");
+        return(dMG);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Entity player = new Entity("YOU", "The Dass Boss",1000, 1000, 1, 77, 50, "Beginners");
-        Entity enemy = new Entity("heuuu", "The Dass Boss",10, 400, 2, 0, 50, "Beginners");
+        Entity player = new Entity("YOU", "The Dass Boss",10000, 1000, 1, 77, 50, "Beginners");
+        Entity enemy = new Entity("heuuu", "The Dass Boss",10, 5000, 2, 0, 50, "Beginners");
         for (int i = 1; i != 5; i++) {
-            int ouch = enemy.attackM1(20);
+            int ouch = enemy.attackM1(20, 1, 2);
             player.dmgReceived(ouch);
-            player.healM2(20, 3);
+            player.healM2(20, 3, 1, 10);
+            player.nukeU1(1,1,1);
         }
     }
 }
